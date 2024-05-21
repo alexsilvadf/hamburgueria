da9cd8263a89f8de0bdb3e8270e59c1b69d8264e
Alex <alex.dasilva@castgroup.com.br>
2024-05-21 09:06:38 -0300

    criando meta tags

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ function checkRestaurantOpen() {
   const data = new Date();
   const hora = data.getHours();
 
-  return hora > 18 && hora < 22;
+  return hora > 0 && hora < 23;
   // true  Restaurante aberto
 }
 
